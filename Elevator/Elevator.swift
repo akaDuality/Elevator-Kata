2848dd8e7e25dc356a61b96050f18393e48cf87a
Mikhail Rubanov <akaDuality@yandex.ru>
2019-12-10 22:12:35 +0300

    A building has floors

---
+++ Elevator/Elevator.swift
@@ -0,0 +1,14 @@
+//
+//  Elevator.swift
+//  Elevator
+//
+//  Created by Mikhail Rubanov on 10.12.2019.
+//  Copyright © 2019 akaDuality. All rights reserved.
+//
+
+import Foundation
+
+struct Building {
+    let floors: Int
+}
+
