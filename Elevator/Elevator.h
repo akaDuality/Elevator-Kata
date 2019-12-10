e6fbb17e9bcbe5eaea0af5a0e0ddde3d198e90bc
Mikhail Rubanov <akaDuality@yandex.ru>
2019-12-10 22:08:24 +0300

    Initial Commit

---
+++ Elevator/Elevator.h
@@ -0,0 +1,19 @@
+//
+//  Elevator.h
+//  Elevator
+//
+//  Created by Mikhail Rubanov on 10.12.2019.
+//  Copyright © 2019 akaDuality. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Elevator.
+FOUNDATION_EXPORT double ElevatorVersionNumber;
+
+//! Project version string for Elevator.
+FOUNDATION_EXPORT const unsigned char ElevatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Elevator/PublicHeader.h>
+
+
